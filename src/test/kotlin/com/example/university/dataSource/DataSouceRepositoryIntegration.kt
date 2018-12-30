48219470b8252388e6684c2a0cf4f4e2dcfc56f3
zhouyao <zhouyao94@qq.com>
2018-12-30 18:55:03 +0800

    添加count字段

--- src/test/kotlin/com/example/university/dataSource/DataSouceRepositoryIntegration.kt
+++ src/test/kotlin/com/example/university/dataSource/DataSouceRepositoryIntegration.kt
@@ -1,5 +1,6 @@
 package com.example.university.dataSource
 
+import org.apache.logging.log4j.LogManager
 import org.assertj.core.api.Assertions.assertThat
 import org.junit.Test
 import org.junit.runner.RunWith
@@ -12,21 +13,40 @@ import org.springframework.test.context.junit4.SpringRunner
 class DataSouceRepositoryIntegration {
 
     @Autowired
-    lateinit var dsRepository:DataSourceRepository
+    lateinit var dsRepository: DataSourceRepository
+
+    var logger = LogManager.getLogger(DataSouceRepositoryIntegration::class.java)
 
     @Test
     fun `thes insert into database`() {
-        dsRepository.save(DataSource(1,"iris","400","4",
-                "university","classification","life"))
+        dsRepository.save(DataSource(1, "iris", "400", "4", "university", "classification", "life"))
+        dsRepository.save(DataSource(2, "iris", "400", "4", "university", "classification", "life"))
+        dsRepository.save(DataSource(3, "iris", "400", "4", "university", "classification", "life"))
+        dsRepository.save(DataSource(4, "iris", "400", "4", "university", "classification", "life"))
+        dsRepository.save(DataSource(5, "iris", "400", "4", "university", "classification", "life"))
+        dsRepository.save(DataSource(6, "iris", "400", "4", "university", "classification", "life"))
+
+
         assertThat(true).isEqualTo(true)
     }
 
 
     @Test
     fun `thest insert `() {
-        val rs  = dsRepository.findAllById(1L)!!
+        val rs = dsRepository.findAllById(1L)!!
         assertThat(rs[0].name).isEqualTo("iris")
     }
+
+    @Test
+    fun `logger the data`() {
+        val rs = dsRepository.findAll()
+        rs.map {
+            logger.info(it)
+        }
+        assertThat(dsRepository.count()).isEqualTo(6)
+
+    }
+
 
 
 }
